ef100a667a909df83fc153fc513652673d03b365
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-06-20 11:43:42 +0100

    fix: single day pick in Date overrides

--- src/components/Managing/DateAvailability.vue
+++ src/components/Managing/DateAvailability.vue
@@ -11,21 +11,28 @@ const props = defineProps({
 
 // console.log("props.timeItems: ", props.timeItems());
 
-const days = ref({
+const defaultStartEndTime = ref({
     start: [props.timeItems()[36], props.timeItems()[20], true, true],
     end: [props.timeItems()[36], props.timeItems()[20], true, true],
 }); // [09:00am(initial value of From), 05:00pm(initial value of To), AM(true)/PM(false) in From time, AM(true)/PM(false) in Until time]
 
 const pickedStartEndDatesArray = ref([new Date(), addDays(new Date(), 0)]);
+const pickedDatesList = ref([]);
 
-watch(pickedStartEndDatesArray, () => {
-    console.log("pickedStartEndDatesArray: ", pickedStartEndDatesArray.value);
+watch(pickedStartEndDatesArray, (newArray) => {
+    console.log("newArray[0]: ", typeof newArray[0]);
+    console.log("newArray[1]: ", typeof newArray[1]);
+    // newArray[1] = newArray[1] ?? 'nothing';
+    console.log("newArray: ", newArray);
+    pickedDatesList.value.push(newArray);
+    console.log("pickedDatesList: ", pickedDatesList.value);
 });
 
 function formatDate(startEndDateArray) {
     const result = [];
     startEndDateArray.forEach(date => {
         if (date !== null) {
+            console.log("typeof date: ", typeof date);
             const day = date.getDate();
             const month = date.getMonth() + 1;
             const year = date.getFullYear();
@@ -38,6 +45,8 @@ function formatDate(startEndDateArray) {
     return result;
 }
 
+
+
 </script>
 
 <template>
@@ -47,22 +56,27 @@ function formatDate(startEndDateArray) {
         :enable-time-picker="false"
         range
     />
-    <v-list width="100%">
+    <v-list
+        v-if="pickedDatesList.length > 0"
+        width="100%"
+    >
+        <template v-for="dates in pickedDatesList">
         <v-list-item
-            v-if="pickedStartEndDatesArray[1] === null"
+            v-if="dates[1] === null"
             base-color=""
             class=""
-            acitve="true"
+            :acitve="true"
             :border="true"
         >
+            <!-- dates: {{ dates[1] }} -->
             <template #prepend>
-                <span class="tw-font-semibold">{{ formatDate(pickedStartEndDatesArray)[0] }}</span>
+                <span class="tw-font-semibold">{{ formatDate(dates)[0] }}</span>
             </template>
             <v-list-item-title>
                 <div class="d-flex flex-row ml-5">
                     <div class="d-flex align-center mr-0">
                         <v-combobox
-                            v-model="days['start'][0]"
+                            v-model="defaultStartEndTime['start'][0]"
                             :items="timeItems()"
                             label="From"
                             variant="outlined"
@@ -77,13 +91,13 @@ function formatDate(startEndDateArray) {
                     <div class="d-flex align-center ml-0">
                         <v-chip
                             class="ml-1 mr-5 px-1"
-                            :color="days['start'][2] ? 'pink' : 'purple'"
-                            @click="days['start'][2] = !(days['start'][2])"
+                            :color="defaultStartEndTime['start'][2] ? 'pink' : 'purple'"
+                            @click="defaultStartEndTime['start'][2] = !(defaultStartEndTime['start'][2])"
                             variant="text"
                             label
                             :ripple="false"
                         >
-                            <span v-if="days['start'][2]" class="">
+                            <span v-if="defaultStartEndTime['start'][2]" class="">
                                 am
                             </span>
                             <span v-else class="">
@@ -93,7 +107,7 @@ function formatDate(startEndDateArray) {
                     </div>
                     <div class="d-flex align-center mr-0">
                         <v-combobox
-                            v-model="days['start'][1]"
+                            v-model="defaultStartEndTime['start'][1]"
                             :items="timeItems()"
                             label="Until"
                             variant="outlined"
@@ -108,13 +122,13 @@ function formatDate(startEndDateArray) {
                     <div class="d-flex align-center ml-0">
                         <v-chip
                             class="ml-1 mr-5 px-1"
-                            :color="days['start'][3] ? 'pink' : 'purple'"
-                            @click="days['start'][3] = !(days['start'][3])"
+                            :color="defaultStartEndTime['start'][3] ? 'pink' : 'purple'"
+                            @click="defaultStartEndTime['start'][3] = !(defaultStartEndTime['start'][3])"
                             variant="text"
                             label
                             :ripple="false"
                         >
-                            <span v-if="days['start'][3]" class="">
+                            <span v-if="defaultStartEndTime['start'][3]" class="">
                                 am
                             </span>
                             <span v-else class="">
@@ -126,11 +140,11 @@ function formatDate(startEndDateArray) {
             </v-list-item-title>
             <template #append>
                 <v-icon :icon="mdiTrashCanOutline" class="ma-0"/>
-            </template>
-
-                            
+            </template>                            
         </v-list-item>
-        <v-list-item v-else v-for="(dateObj, i) in formatDate(pickedStartEndDatesArray)">
+        </template>
+
+        <!-- <v-list-item v-else v-for="(dateObj, i) in formatDate(pickedStartEndDatesArray)">
             <v-col>
                 <v-row v-if="i === 0">
                     From: {{ dateObj }}
@@ -139,7 +153,7 @@ function formatDate(startEndDateArray) {
                     Until: {{ dateObj }}
                 </v-row>
             </v-col>
-        </v-list-item>
+        </v-list-item> -->
 
     </v-list>
 
